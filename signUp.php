1c54d2649edf01669f36b3da5c8a90b6e521e402
Satyajit <satyajitmanna35@gmail.com>
2025-07-03 12:44:39 +0530

    add signup and signin pages

--- signUp.php
+++ signUp.php
@@ -1,31 +1,33 @@
 <?php
 require('./dbconfig.php');
 
-// $userName= $_POST['username'];
-// $usergmail= $_POST['gmail'];
-// $userpassword= $_POST['password'];
-// $usermobile= $_POST['mobile'];
+$userName= $_POST['username'];
+$usergmail= $_POST['gmail'];
+$userpassword= $_POST['password'];
+$usermobile= $_POST['mobile'];
 
-$userName= "Satyajit";
-$usergmail= "satyajitmanna35@gmail.com";
-$userpassword= "Satyajit@1234";
-$usermobile= 8967104521;
-$query="SELECT * FROM userinformation WHERE Gmail='$usergmail' ";
+// $userName = "Satyajit";
+// $usergmail = "satyajitmanna35@gmail.com";
+// $userpassword = "Satyajit@1234";
+// $usermobile = 8967104521;
 
-$result=mysqli_query($conn,$query);
-if (mysqli_num_rows($result)>0) {
-    echo "Already exists User" ;
+$query = "SELECT * FROM userinformation WHERE Gmail='$usergmail' ";
+
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) {
+    echo "Already exists User";
     mysqli_close($conn);
-}else {
-        $query="INSERT INTO userinformation(UserName,Gmail,Password,Mobile) 
+} else {
+    $query = "INSERT INTO userinformation(UserName,Gmail,Password,Mobile) 
 VALUES
 ('$userName','$usergmail','$userpassword','$usermobile')";
 
-$result= mysqli_query($conn,$query);
+    $result = mysqli_query($conn, $query);
 
-if (mysqli_affected_rows($conn)>0) {
-    echo "usersets" ;
-    mysqli_close($conn);
-}else echo "users not set";
+    if (mysqli_affected_rows($conn) > 0) {
+        echo "usersets";
+        mysqli_close($conn);
+    } else
+        echo "users not set";
 }
 ?>
